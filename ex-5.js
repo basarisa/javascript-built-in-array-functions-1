f6979189ab2bf3285c9d4131998345420c6efa84
Arisa Teandum <basarisa.1302@gmail.com>
2024-10-09 15:05:54 +0700

    feat: built-in array functions 1 assignment

--- ex-5.js
+++ ex-5.js
@@ -6,7 +6,9 @@ const students = [
 ];
 
 function getAverageStudentScore(students) {
-  // Start coding here
+  let totelScore = students.reduce((acc, student) => acc + student.score, 0);
+
+  return totelScore / students.length;
 }
 
-getAverageStudentScore(students); // Output: 87.5
+console.log(getAverageStudentScore(students)); // Output: 87.5
